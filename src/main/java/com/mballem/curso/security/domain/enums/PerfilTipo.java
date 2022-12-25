12290e6702b89837f840958e9e0c0248945e8daf
= <vinny.parker@gmail.com>
2022-12-25 16:26:57 -0300

    start

---
+++ src/main/java/com/mballem/curso/security/domain/enums/PerfilTipo.java
@@ -0,0 +1,23 @@
+package com.mballem.curso.security.domain.enums;
+
+public enum PerfilTipo {
+    ADMIN(1, "ADMIN"),
+    MEDICO(2, "MEDICO"),
+    PACIENTE(3, "PACIENTE");
+
+    private long cod;
+    private String desc;
+
+    private PerfilTipo(long cod, String desc) {
+        this.cod = cod;
+        this.desc = desc;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
